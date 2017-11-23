dc942f0acf3032705221f4017b82c6b6b19aa752
Ivan Bogun <gnattuha@gmail.com>
2017-11-23 10:55:20 -0800

    added Inception model with inference

--- perturbations/deep_fool.py
+++ perturbations/deep_fool.py
@@ -2,7 +2,10 @@
 Implementation of the Deep fool paper.
 """
 import tensorflow as tf
+from inception_preprocessing import preprocess_image
+from inception_resnet_v2 import inception_resnet_v2, inception_resnet_v2_arg_scope
 
+slim = tf.contrib.slim
 
 class MulticlassClassifier(object):
     """Multiclass image classifier.
@@ -28,6 +31,62 @@ class MulticlassClassifier(object):
     def num_classes(self):
         """Returns output number of classes."""
         pass
+
+class InceptionImageNetClassifier(MulticlassClassifier):
+
+    def __init__(self, path_to_checkpoint):
+        """Creates an instance of the classifier."""
+        self._checkpoint_path = path_to_checkpoint
+        self._input_dims = (299, 299, 3)
+        # Label 0 is for background.
+        self._num_classes = 1001
+
+    def input_dims(self):
+        """Returns input size as a tuple"""
+        return self._input_dims
+
+    def num_classes(self):
+        """Returns output number of classes as a scalar."""
+        return self._num_classes
+
+    def read_and_preprocess(self, image_path):
+        """Reads and preprocesses the image from a path."""
+
+        with tf.Graph().as_default():
+            image_encoded = tf.gfile.FastGFile(image_path, 'rb').read()
+            image_decoded = tf.image.decode_jpeg(image_encoded, channels=3)
+
+            input_dims = self.input_dims()
+            processed_image_tensor = preprocess_image(image_decoded, input_dims[0],
+                                                      input_dims[1], is_training=False)
+
+            with tf.Session() as sess:
+                processed_image = sess.run([processed_image_tensor])
+                return processed_image[0]
+
+
+    def __call__(self, image):
+        """Image is assumed to be RBD image."""
+
+        with tf.Graph().as_default():
+            with slim.arg_scope(inception_resnet_v2_arg_scope()):
+                processed_image = tf.placeholder(tf.float32, shape = self.input_dims())
+                processed_images = tf.expand_dims(processed_image, 0)
+
+                # Note(bogun): The class 0 is used to represent "non of the above" class also known as background.
+                logits, _ = inception_resnet_v2(processed_images, num_classes=self.num_classes(), is_training=False)
+                probabilities = tf.nn.softmax(logits)
+
+                init_fn = slim.assign_from_checkpoint_fn(self._checkpoint_path, slim.get_model_variables('InceptionResnetV2'))
+
+                with tf.Session() as sess:
+                    init_fn(sess)
+                    probabilities = sess.run([probabilities], feed_dict = {processed_image : image})
+
+                    probs = probabilities[0]
+                    return probs[0]
+    
+
 
 class DeepFool(object):
     """
--- perturbations/deep_fool_test.py
+++ perturbations/deep_fool_test.py
@@ -1,16 +1,24 @@
 """
 Unit tests.
 """
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import deep_fool
+import numpy as np
 
 class DeepFoolTest(tf.test.TestCase):
     """Test suite for deep fool."""
 
     def setUp(self):
         """Setup."""
-        self._model = deep_fool.MulticlassClassifier()
+
+        checkpoint_dir = "../checkpoints/"
+        checkpoint_file = "inception_resnet_v2_2016_08_30.ckpt"
+        path = os.path.join(checkpoint_dir, checkpoint_file)
+
+        self._image_path = '../images/n02099601_10.JPEG'
+        self._model = deep_fool.InceptionImageNetClassifier(path)
 
     def test_size(self):
         """Tests the size of the model."""
@@ -19,20 +27,21 @@ class DeepFoolTest(tf.test.TestCase):
             input_size = self._model.input_dims()
 
             # Tensor is expected to be image tensor
-            self.assertEqual(input_size.size(), 2)
+            self.assertEqual(len(input_size), 3)
             self.assertGreater(input_size[0], 0)
             self.assertGreater(input_size[1], 0)
 
+    def test_read_and_preprocess(self):
+        """Tests if image can be read and preprocessed."""
+        image = self._model.read_and_preprocess(self._image_path)
+        self.assertEqual(np.shape(image), self._model.input_dims())
+
+
     def test_inference(self):
         """Tests if the inference can be performed."""
-        with self.test_session():
-            input_size = self._model.input_dims()
-            # Create a tensor of the given shape.
-            zeros = tf.zeros(input_size, tf.float32)
-            output_size = self._model.num_classes()
-
-            output = self._model(zeros)
-            self.assertEquals(output.size(), output_size)
+        image = self._model.read_and_preprocess(self._image_path)
+        probs = self._model(image)
+        self.assertEqual(len(probs), self._model.num_classes())
 
 
 
